6ee9cfc140f22d4183d1f786a7f172999a544364
Henrique Rodrigues Araujo <55699671+HenriqueRodriguesA@users.noreply.github.com>
2023-11-03 15:33:35 -0300

    Update constMP.py

--- constMP.py
+++ constMP.py
@@ -5,15 +5,15 @@
 
 # With public addresses (in the same region of the cloud)
 # The last one is not fixed and must be changed each time the lab is restarted.
-PEERS_SAME_REGION = ['3.219.23.96','34.226.166.158','44.198.151.245','44.219.197.47','44.220.0.53','54.175.18.26']
+PEERS_SAME_REGION = ['44.220.2.241','44.206.203.140','54.88.55.186','107.23.70.91','3.232.114.77','54.163.114.25']
 
 # With public addresses (in two separate regions - last two servers in Oregon)
-PEERS_TWO_REGIONS = ['3.219.23.96','34.226.166.158','44.198.151.245','44.219.197.47','44.239.194.163','52.88.79.71']
+PEERS_TWO_REGIONS = ['44.220.2.241','44.206.203.140','54.88.55.186','107.23.70.91','52.89.31.9','52.35.213.24']
 
 
 PEER_UDP_PORT = 4567
 PEER_TCP_PORT = 5679
 N = 6   # Number of peers
-SERVER_ADDR ='3.219.23.96'
+SERVER_ADDR ='44.220.2.241'
 SERVER_PORT = 5678
 
